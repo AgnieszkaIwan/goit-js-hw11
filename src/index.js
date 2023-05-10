836f2868a23d62a9e89f1ab6352690fd654558dc
Agnieszka Iwan <agnieszka.iwan2@gmail.com>
2023-05-10 22:00:00 +0200

    .

--- src/index.js
+++ src/index.js
@@ -7,12 +7,28 @@ const API_URL = 'https://pixabay.com/api/';
 
 const searchForm = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+let page = 1;
+let currentPage = 1;
 
 searchForm.addEventListener('submit', e => {
   e.preventDefault();
+  currentPage = 1; // reset currentPage to 1 for new search
 
   const searchQuery = e.target.elements.searchQuery.value.trim();
 
+  searchImages(searchQuery, currentPage);
+});
+
+loadMoreBtn.addEventListener('click', () => {
+  currentPage++; // increase currentPage for next page of results
+
+  const searchQuery = searchForm.elements.searchQuery.value.trim();
+
+  searchImages(searchQuery, currentPage);
+});
+
+function searchImages(searchQuery, page) {
   axios
     .get(API_URL, {
       params: {
@@ -21,11 +37,12 @@ searchForm.addEventListener('submit', e => {
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
+        per_page: 40,
+        page: page,
       },
     })
     .then(response => {
       const photos = response.data.hits;
-
       if (photos.length === 0) {
         Notiflix.Notify.failure(
           'Sorry, there are no images matching your search query. Please try again.'
@@ -33,35 +50,58 @@ searchForm.addEventListener('submit', e => {
         return;
       }
 
-      gallery.innerHTML = '';
+      if (page === 1) {
+        gallery.innerHTML = '';
+      }
 
       photos.forEach(photo => {
         const card = `
-        <div class="photo-card">
-          <img src="${photo.webformatURL}" alt="${photo.tags}" loading="lazy" />
-          <div class="info">
-            <p class="info-item">
-              <b>Likes:</b> ${photo.likes}
-            </p>
-            <p class="info-item">
-              <b>Views:</b> ${photo.views}
-            </p>
-            <p class="info-item">
-              <b>Comments:</b> ${photo.comments}
-            </p>
-            <p class="info-item">
-              <b>Downloads:</b> ${photo.downloads}
-            </p>
-          </div>
-        </div>
-      `;
+    <div class="photo-card">
+      <img src="${photo.webformatURL}" alt="${photo.tags}" loading="lazy" />
+      <div class="info">
+        <p class="info-item">
+          <b>Likes:</b> ${photo.likes}
+        </p>
+        <p class="info-item">
+          <b>Views:</b> ${photo.views}
+        </p>
+        <p class="info-item">
+          <b>Comments:</b> ${photo.comments}
+        </p>
+        <p class="info-item">
+          <b>Downloads:</b> ${photo.downloads}
+        </p>
+      </div>
+    </div>
+  `;
 
         gallery.insertAdjacentHTML('beforeend', card);
       });
 
+      if (
+        photos.length < 40 ||
+        gallery.children.length >= response.data.totalHits
+      ) {
+        // ukrycie przycisku "Load more" jeśli dotarliśmy do końca kolekcji
+        loadMoreBtn.style.display = 'none';
+        Notiflix.Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+      } else {
+        // wyświetlenie przycisku "Load more" w przeciwnym razie
+        loadMoreBtn.style.display = 'block';
+      }
+
       Notiflix.Notify.success(
         `Found ${photos.length} images for "${searchQuery}"`
       );
+
+      // // show loadMoreBtn if there are more pages of results
+      // if (response.data.totalHits > currentPage * 40) {
+      //   loadMoreBtn.style.display = 'block';
+      // } else {
+      //   loadMoreBtn.style.display = 'none';
+      // }
     })
     .catch(error => {
       console.error(error);
@@ -69,71 +109,4 @@ searchForm.addEventListener('submit', e => {
         'Oops, something went wrong. Please try again later.'
       );
     });
-});
-
-async function fetchImages() {
-  try {
-    const response = await axios.get(API_URL, {
-      params: {
-        key: API_KEY,
-        q: searchQuery,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        per_page: 40, //liczba obiektów na stronie
-        page: pageNumber, //numer strony
-      },
-    });
-
-    const hits = response.data.hits;
-    const totalHits = response.data.totalHits;
-
-    if (hits.length === 0) {
-      // pokaz powiadomienie, ze nie znaleziono obrazkow
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    } else {
-      //dodaj obrazy do galerii
-      const gallery = document.querySelector('.gallery');
-      hits.forEach(hit => {
-        const card = createPhotoCard(hit);
-        gallery.insertAdjacentHTML('beforeend', card);
-      });
-
-      //jeśli pobrano mniej obrazków niż per_page - koniec kolekcji
-      if (pageNumber * 40 >= totalHits) {
-        loadMoreBtn.style.display = 'none';
-        Notiflix.Notify.info(
-          "We're sorry, but you've reached the end of search results."
-        );
-      } else {
-        //w innym przypadku pokaz przycisk
-        loadMoreBtn.style.display = 'block';
-      }
-    }
-  } catch (error) {
-    console.log(error);
-  }
 }
-
-const loadMoreBtn = document.getElementById('load-more');
-
-let pageNumber;
-
-loadMoreBtn.addEventListener('click', () => {
-  pageNumber += 1;
-  fetchImages();
-});
-
-// loadMoreBtn.style.display = 'none'; // początkowo ukryj przycisk
-
-// if (response.data.hits.length === 0) {
-//   // wyświetl powiadomienie o braku wyników i ukryj przycisk
-//   gallery.innerHTML = '<p>No results found.</p>';
-//   loadMoreBtn.style.display = 'none';
-// } else {
-//   // wyświetl kolejne obrazy i pokaż przycisk "load more"
-//   gallery.insertAdjacentHTML('beforeend', markup);
-//   loadMoreBtn.style.display = 'block';
-// }
